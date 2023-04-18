c3d2175a6c40bd4433f7c5fac51f0aeb02915a2d
Sara <sara.correagarcia@gmail.com>
2023-04-18 17:16:54 -0400

    deleted intermediate file

---
+++ Devoir semaine 4 - Python.py
@@ -0,0 +1,55 @@
+#Devoir semaine 4 - Python. Creer une interface utilisateur permettant de calculer le périmètre et l'aire d'un triangle rectangle
+# à partir  de la base et de la hauteur du triangle. 
+
+# Importer la bibliothèque tkinter pour créer une interface graphique
+from tkinter import *
+# Importer la fonction racine carrée de la bibliothèque mathématique pour le calcul du périmètre
+from math import sqrt
+
+# Define functions Aire and Perim
+def Aire():
+    base = float(entreeBase.get())
+    hauteur = float(entreeHauteur.get())
+    aire = (base * hauteur) / 2
+    txtPerim.configure(text="") # Effacer le texte précédent affiché pour le périmètre
+    txtAire.configure(text="{:.2f}".format(aire)) # Afficher l'aire calculée avec deux décimales
+    txtCm.configure(text="Aire:")
+    
+def Perim():
+    base = float(entreeBase.get())
+    hauteur = float(entreeHauteur.get())
+    perim = base + hauteur + sqrt(base**2 +hauteur**2)
+    txtAire.configure(text="")
+    txtPerim.configure(text="{:.2f}".format(perim))
+    txtCm.configure(text="Perimetre:")
+
+# Define window
+fen1 = Tk()
+fen1.title("Triangle")
+
+# Create labels
+label_b = Label(fen1, text = "base:")
+label_b.grid(row = 0, column = 0)
+label_h = Label(fen1, text = "hauteur:")
+label_h.grid(row = 1, column = 0)
+txtCm = Label(fen1, text = "--:")
+txtCm.grid(row = 2, column = 0)
+
+txtAire = Label(fen1, text="")
+txtAire.grid(row=2, column=1)
+txtPerim = Label(fen1, text="")
+txtPerim.grid(row=2, column=1)
+
+# Create entry text zones for base and height
+entreeBase = Entry(fen1)
+entreeBase.grid(row = 0, column = 1)
+entreeHauteur = Entry(fen1)
+entreeHauteur.grid(row = 1, column = 1)
+
+# Create buttons to obtain calculations
+boutonAire = Button(fen1, text = "Aire", command = Aire)
+boutonAire.grid(row = 3, column = 0)
+boutonPerim = Button(fen1, text = "Perimetre", command = Perim)
+boutonPerim.grid(row = 3, column = 1)
+
+fen1.mainloop()
--- gui_script.py
+++ gui_script.py
@@ -1,35 +1,69 @@
-from tkinter import * # Load library
+#Devoir semaine 4 - Python. Creer une interface utilisateur permettant de calculer le périmètre et l'aire d'un triangle rectangle
+# à partir  de la base et de la hauteur du triangle. 
+
+# Importer las bibliothèques tkinter pour créer une interface graphique, et math pour sqrt
+from tkinter import *
 from math import sqrt
 
-# Define functions Aire and Perim
+# Définir les fonctions Aire et Perim
 def Aire():
+    # Récupérer les valeurs entrées pour la base et la hauteur
     base = float(entreeBase.get())
     hauteur = float(entreeHauteur.get())
+    # Calculer l'aire du triangle
     aire = (base * hauteur) / 2
+    # Effacer le texte précédent affiché pour le périmètre
+    txtPerim.configure(text="")
+    # Afficher l'aire calculée avec deux décimales
+    txtAire.configure(text="{:.2f}".format(aire))
+    # Changer le texte du label pour "Aire"
+    txtCm.configure(text="Aire:")
+
 def Perim():
+    # Récupérer les valeurs entrées pour la base et la hauteur
     base = float(entreeBase.get())
     hauteur = float(entreeHauteur.get())
+    # Calculer le périmètre du triangle
     perim = base + hauteur + sqrt(base**2 +hauteur**2)
+    # Effacer le texte précédent affiché pour l'aire
+    txtAire.configure(text="")
+    # Afficher le périmètre calculé avec deux décimales
+    txtPerim.configure(text="{:.2f}".format(perim))
+    # Changer le texte du label pour "Périmètre"
+    txtCm.configure(text="Périmètre:")
 
-# Define window
+# Créer une fenêtre principale
 fen1 = Tk()
+# Donner un titre à la fenêtre
+fen1.title("Triangle")
 
-# Create labels
+# Créer les étiquettes pour l'entrée de la base et de la hauteur
 label_b = Label(fen1, text = "base:")
-label_b.grid(row = 0, column = 0)
-label_h = Label(fen1, text = "hauteur")
-label_h.grid(row = 0, column = 1)
+label_b.grid(row = 0, column = 0, sticky=E)
+label_h = Label(fen1, text = "hauteur:")
+label_h.grid(row = 1, column = 0, sticky=E)
+# Créer un label pour afficher le résultat (air ou périmètre)
+txtCm = Label(fen1, text = "--:")
+txtCm.grid(row = 2, column = 0, sticky=E)
 
-# Create entry text zones for base and height
+# Créer un label pour afficher le résultat de l'aire
+txtAire = Label(fen1, text="")
+txtAire.grid(row=2, column=1)
+# Créer un label pour afficher le résultat du périmètre
+txtPerim = Label(fen1, text="")
+txtPerim.grid(row=2, column=1)
+
+# Créer des zones d'entrée pour la base et la hauteur
 entreeBase = Entry(fen1)
 entreeBase.grid(row = 0, column = 1)
 entreeHauteur = Entry(fen1)
 entreeHauteur.grid(row = 1, column = 1)
 
-# Create buttons to obtain calculations
+# Créer des boutons pour lancer les calculs
 boutonAire = Button(fen1, text = "Aire", command = Aire)
-boutonAire.grid(row = 4, column = 0)
-boutonPerim = Button(fen1, text = "Perimetre", command = Perim)
-boutonPerim.grid(row = 4, column = 1)
+boutonAire.grid(row = 3, column = 0)
+boutonPerim = Button(fen1, text = "Périmètre", command = Perim)
+boutonPerim.grid(row = 3, column = 1)
 
+# Démarrer la boucle et la fenêtre
 fen1.mainloop()
